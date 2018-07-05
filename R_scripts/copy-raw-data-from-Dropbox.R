a245a89ff711b4c311a3b7235be68ea6255e0df6
Whalen <whalend@users.noreply.github.com>
2018-07-05 13:45:43 -0400

    Update for data entry file naming.

--- R_scripts/copy-raw-data-from-Dropbox.R
+++ R_scripts/copy-raw-data-from-Dropbox.R
@@ -1,31 +1,32 @@
 ## Copy raw data from Dropbox to project directory ----
+library(readxl)
 
-cogon_data <- read.csv("~/Dropbox (UF)/SERDP-Project/data/cogon-data.csv")
+cogon_data <- read_excel("~/Dropbox (UF)/SERDP-Project/data/cogon-data-entry.xlsx", sheet = 1)
 write.csv(cogon_data, "data/raw_data/cogon-data.csv", row.names = F)
 
-densiometer <- read.csv("~/Dropbox (UF)/SERDP-Project/data/densiometer-canopy-cover.csv")
+densiometer <- read_excel("~/Dropbox (UF)/SERDP-Project/data/densiometer-canopy-cover-data-entry.xlsx", sheet = 1)
 write.csv(densiometer, "data/raw_data/densiometer-data.csv", row.names = F)
 
-dung <- read.csv("~/Dropbox (UF)/SERDP-Project/data/dung.csv")
+dung <- read_excel("~/Dropbox (UF)/SERDP-Project/data/dung-data-entry.xlsx", sheet = 1)
 write.csv(dung, "data/raw_data/dung.csv", row.names = F)
 
-plot_info <- read.csv("~/Dropbox (UF)/SERDP-Project/data/plot-info.csv")
-write.csv(plot_info, "data/raw_data/plot-info.csv", row.names = F)
+# plot_info <- read.csv("~/Dropbox (UF)/SERDP-Project/data/plot-info.csv")
+# write.csv(plot_info, "data/raw_data/plot-info.csv", row.names = F)
 
-quadrat1m <- read.csv("~/Dropbox (UF)/SERDP-Project/data/quadrat1m.csv")
+quadrat1m <- read_excel("~/Dropbox (UF)/SERDP-Project/data/quadrat1m-data-entry.xlsx", sheet = 1)
 write.csv(quadrat1m, "data/raw_data/quadrat1m.csv", row.names = F)
 
-quadrat25cm <- read.csv("~/Dropbox (UF)/SERDP-Project/data/quadrat25cm.csv")
+quadrat25cm <- read_excel("~/Dropbox (UF)/SERDP-Project/data/quadrat25cm-data-entry.xlsx", sheet = 1)
 write.csv(quadrat25cm, "data/raw_data/quadrat25cm.csv", row.names = F)
 
-species1m <- read.csv("~/Dropbox (UF)/SERDP-Project/data/species1m.csv")
+species1m <- read_excel("~/Dropbox (UF)/SERDP-Project/data/species1m-data-entry.xlsx", sheet = 1)
 write.csv(species1m, "data/raw_data/species1m.csv", row.names = F)
 
-woody_subplot <- read.csv("~/Dropbox (UF)/SERDP-Project/data/subplot-woody-species.csv")
+woody_subplot <- read_excel("~/Dropbox (UF)/SERDP-Project/data/subplot-woody-species-data-entry.xlsx", sheet = 1)
 write.csv(woody_subplot, "data/raw_data/subplot-woody-species.csv", row.names = F)
 
-ticks <- read.csv("~/Dropbox (UF)/SERDP-Project/data/ticks.csv")
+ticks <- read_excel("~/Dropbox (UF)/SERDP-Project/data/ticks-data-entry.xlsx", sheet = 1)
 write.csv(ticks, "data/raw_data/ticks.csv", row.names = F)
 
-trees <- read.csv("~/Dropbox (UF)/SERDP-Project/data/tree-data.csv")
+trees <- read_excel("~/Dropbox (UF)/SERDP-Project/data/tree-data-entry.xlsx", sheet = 1)
 write.csv(trees, "data/raw_data/trees.csv", row.names = F)
