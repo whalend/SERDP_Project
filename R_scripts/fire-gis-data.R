40a08f42602cb977f3a2168043abfeed9afd55ac
Whalen <whalend@users.noreply.github.com>
2018-07-31 13:35:54 -0400

    Break down Fort Gordon fire data.

--- R_scripts/fire-gis-data.R
+++ R_scripts/fire-gis-data.R
@@ -383,3 +383,50 @@ ggplot(rx_merged_df) +
       coord_equal() +
       scale_fill_gradientn(colors = inferno(n = n_distinct(rx_merged_df$fire_fiscal_year))) +
       theme_bw()
+
+
+# Fort Gordon ####
+gordon_fire <- readOGR("data/FtGordon/wildland_fire.shp")
+summary(gordon_fire)
+
+gordon_fire@data$DATE_EVENT <- as.Date(
+      as.character(gordon_fire@data$DATE_EVENT),
+      format = "%Y%m%d")
+
+gordon_fire@data <- mutate(gordon_fire@data,
+                      burnYr = lubridate::year(DATE_EVENT),
+                      burnMo = lubridate::month(DATE_EVENT),
+                      burnMo2 = lubridate::round_date(DATE_EVENT, "month"))
+summary(gordon_fire@data)
+
+yr <- seq(1995,2018,1)
+library(raster)
+for(y in yr){
+      df <- gordon_fire[gordon_fire@data$burnYr == y,]
+      centroids <- rgeos::gCentroid(df, byid = T)
+      centroids <- SpatialPointsDataFrame(
+            centroids, #SpatialPoints object
+            df[1]@data #Dataframe
+      )
+      names(centroids) <- "ptFID"
+      centroids <- raster::intersect(centroids, gordon_fire)
+      df@data <- left_join(
+            df@data,
+            ## Summarise data to calculate number of fires and fire frequency
+            gordon_fire@data %>%
+                  filter(burnYr >= 2003) %>%
+                  group_by(OBJECTID) %>%
+                  summarise(fires = length(OBJECTID),
+                            fri15yr = (2018-2003)/fires,
+                            lastBrn = max(burnYr)
+                            # frindex = (1/fri15yr)/range(burnYr)
+                  ),
+            by = "OBJECTID"
+      )
+      writeOGR(df,
+               dsn = "data/FtGordon/",
+               layer = paste("gordon_fire_", y, sep=""),
+               driver = "ESRI Shapefile", overwrite_layer = T)
+      assign(paste("gordon_fire_", y, sep=""), df)
+}
+detach("package:raster", unload=TRUE)
