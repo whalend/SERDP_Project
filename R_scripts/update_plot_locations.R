eeb6611e61d6e3e89bd642ae7529939a3ae08aea
Whalen <whalend@users.noreply.github.com>
2018-07-31 13:34:00 -0400

    Add points from Fort Benning.

--- R_scripts/update_plot_locations.R
+++ R_scripts/update_plot_locations.R
@@ -32,19 +32,27 @@ flist <- list.files("/Volumes/GARMIN/Garmin/GPX", pattern = "-18", full.names = 
 # flist <- strsplit(flist, ".gpx")
 waypt <- 1
 for(f in flist){
-      assign(paste("waypts",waypt,sep = ""), readOGR(dsn = paste(f), layer = "waypoints"))
+      i = readOGR(dsn = paste(f), layer = "waypoints")
+      i@data = i@data[,1:5]
+      assign(paste("waypts",waypt,sep = ""), i)
       waypt = waypt+1
 }
 
 blanding_k1 <- readOGR("/Volumes/GARMIN/Garmin/GPX/BlandingK1.gpx", layer = "waypoints")
+blanding_k1@data <- blanding_k1@data[,1:5]
 blanding_m1 <- readOGR("/Volumes/GARMIN/Garmin/GPX/BlandingM1.gpx", layer = "waypoints")
+blanding_m1@data <- blanding_m1@data[,1:5]
 
 jacks_j1 <- readOGR("/Volumes/GARMIN/Garmin/GPX/JacksonJ1.gpx", layer = "waypoints")
+jacks_j1@data <- jacks_j1@data[,1:5]
 jacks_k1 <- readOGR("/Volumes/GARMIN/Garmin/GPX/JacksonK1.gpx", layer = "waypoints")
+jacks_k1@data <- jacks_k1@data[,1:5]
 jacks_l1 <- readOGR("/Volumes/GARMIN/Garmin/GPX/JacksonL1.gpx", layer = "waypoints")
+jacks_l1@data <- jacks_l1@data[,1:5]
 jacks_m1 <- readOGR("/Volumes/GARMIN/Garmin/GPX/JacksonM1.gpx", layer = "waypoints")
+jacks_m1@data <- jacks_m1@data[,1:5]
 
-new_waypts <- rbind(waypts1,waypts2,waypts3,waypts4,waypts5,waypts6,waypts7,waypts8,waypts9,waypts10,waypts11,waypts12,waypts13,waypts14,waypts15,waypts16,waypts17,waypts18, blanding_k1,blanding_m1, jacks_j1,jacks_k1,jacks_l1,jacks_m1)
+new_waypts <- rbind(waypts1,waypts2,waypts3,waypts4,waypts5,waypts6,waypts7,waypts8,waypts9,waypts10,waypts11,waypts12,waypts13,waypts14,waypts15,waypts16,waypts17,waypts18,waypts19,waypts20,waypts21,waypts22,waypts23, blanding_k1,blanding_m1, jacks_j1,jacks_k1,jacks_l1,jacks_m1)
 
 # waypts <-
 # readOGR("/Volumes/GARMIN/Garmin/GPX/Waypoints_24-MAY-18.gpx", layer = "waypoints")
@@ -78,6 +86,8 @@ all_plots <- rbind(all_plots, new_waypts)
 all_plots <- remove.duplicates(all_plots)
 
 summary(all_plots)
+
+sort(all_plots$name)
 
 
 # flame_height_locations <- all_plots[all_plots$descriptio!="Semipermanent plot",]
