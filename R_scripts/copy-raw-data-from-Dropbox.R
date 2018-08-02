c484d096992a85da4b572c4967dfaf364dccd7c6
Whalen <whalend@users.noreply.github.com>
2018-08-01 21:22:51 -0400

    Add new line and end of file.

--- R_scripts/copy-raw-data-from-Dropbox.R
+++ R_scripts/copy-raw-data-from-Dropbox.R
@@ -33,3 +33,4 @@ write.csv(trees, "data/raw_data/trees.csv", row.names = F)
 
 plot_visits <- read_excel("~/Dropbox (UF)/SERDP-Project/data/plot-visit-data-entry.xlsx", sheet = 1)
 write.csv(plot_visits, "data/raw_data/plot_visit.csv", row.names = F)
+
