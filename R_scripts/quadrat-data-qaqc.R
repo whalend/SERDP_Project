4fec54cf63c382bfa7151adc4c55c0be7c3c66f5
Whalen <whalend@users.noreply.github.com>
2018-09-06 15:38:32 -0400

    Update data QA/QC for 1 meter quadrat.

--- R_scripts/quadrat-data-qaqc.R
+++ R_scripts/quadrat-data-qaqc.R
@@ -1,5 +1,11 @@
 #' # Script for doing QA/QC on the 1m and 25cm quadrat data
 
+#+ load packages ####
+library(plyr); library(dplyr);
+library(readr)
+library(stringi)
+library(ggplot2)
+
 #' ## Quadrat Data
 #' Percent cover
 #'
@@ -19,40 +25,84 @@ plot_visit_data <- read_csv("data/processed_data/plot_visit_data.csv")
 
 #+ 1m quadrat data ####
 quadrat_data <- read_csv("data/raw_data/quadrat1m.csv")
-quadrat_data$visit_date <- as.Date(quadrat_data$date, format = "%m/%d/%Y")
-# quadrat_data$date <- gsub("-","", as.character(quadrat_data$visit_date))
+summary(quadrat_data)
+str(quadrat_data)
 
-# as.Date(gsub("-","", as.character(quadrat_data$date)), format = "%Y%m%d")
+#' Data Issues
+#' * ~~4 `NA's` in 'date'~~
+#' * ~~116 `NA's` in 'woody_veg_ht1~~
+#' * ~~'herb_veg_ht' & 'pct_green' columns are reading in as character when they should be numeric~~
+#' * 3 `NA's` in 'pct_litter'
+#' * 63 `NA's` in 'pct_bare'
+#' * ~~need unique plot id~~
+#' * ~~need to filter out data from pilot plot visits, i.e. before 20170601~~
+#'
+#' Since we took up to three measurements of vegetation height and litter depth we deemed that if there wasn't anything meeting that category then the first measurement would be 0 while the remaining two would be recorded as `NA`. There shouldn't be any `NA's` in the first measurement column for these variables. Also, there should rarely (never?) be `NA's` for measures of percent cover.
 
-# plot_visit_data <- left_join(plot_visit_data,
-#           quadrat_data %>% select(installation, plot_id, visit_date) %>%
-#                 filter(visit_date>"2017-06-01") %>%
-#                 unique(.))
-# plot_visit_data <- plot_visit_data %>%
-#       mutate(date = gsub("-","", visit_date),
-#              visit_id = paste(date, installation, plot_id, sep = "_"))
+#+ add unique plot id and investigate date NA's ####
+quadrat_data <- quadrat_data %>%
+      mutate(plot_id = paste(installation, plot_id, sep = " "))
+unique(quadrat_data$plot_id)# "extra" plots from design tests
+unique(plot_visit_data$plot_id)
+quadrat_data$plot_id[quadrat_data$plot_id=="blanding cogan"] <- "blanding theater_cogon"
 
-# write_csv(plot_visit_data, "data/plot_visit_data.csv")
+filter(quadrat_data, is.na(date))$plot_id
+filter(plot_visit_data, plot_id=="blanding c1")
+quadrat_data$date[is.na(quadrat_data$date)] <- 20180516
 
-lubridate::year(quadrat_data$visit_date)
-lubridate::month(quadrat_data$visit_date)
-lubridate::day(quadrat_data$visit_date)
+quadrat_data <- quadrat_data %>%
+      filter(date > 20170601) # discard design test data
 
-write_csv(quadrat_data, "data/quadrat1m.csv")
-quadrat_data <- filter(quadrat_data, visit_date > "2017-06-01")# discard early data collection at Camp Blanding
+unique(plot_visit_data$plot_id) %in% unique(quadrat_data$plot_id)
+#+ convert herb_veg_ht to numeric ###
 
 summary(quadrat_data)
 
+#+ investigate herb_veg_ht as character
+sort(unique(quadrat_data$herb_veg_ht1))# "82I"
+sort(unique(quadrat_data$herb_veg_ht2))# "74I"
+sort(unique(quadrat_data$herb_veg_ht3))# "70I" & "nA"
+
+filter(quadrat_data, herb_veg_ht1=="82I")$plot_id
+quadrat_data$herb_veg_ht1[quadrat_data$herb_veg_ht1=="82I"] <- 82
+filter(quadrat_data, herb_veg_ht2=="74I")$plot_id
+quadrat_data$herb_veg_ht2[quadrat_data$herb_veg_ht2=="74I"] <- 74
+filter(quadrat_data, herb_veg_ht3=="70I")$plot_id
+quadrat_data$herb_veg_ht3[quadrat_data$herb_veg_ht3=="70I"] <- 70
+filter(quadrat_data, herb_veg_ht3=="nA")$plot_id
+
+quadrat_data <- quadrat_data %>%
+      mutate(herb_veg_ht1 = as.numeric(herb_veg_ht1),
+       herb_veg_ht2 = as.numeric(herb_veg_ht2),
+       herb_veg_ht3 = as.numeric(herb_veg_ht3))
+
+summary(quadrat_data)
+
+#+ check NA's in ht1 columns and convert to 0 as appropriate ####
+
+summary(filter(quadrat_data, is.na(woody_veg_ht1)))
+# View(filter(quadrat_data, is.na(woody_veg_ht1), date < 20180101))
 quadrat_data$woody_veg_ht1[is.na(quadrat_data$woody_veg_ht1)] <- 0
-# quadrat_data$woody_veg_ht1 <- as.numeric(quadrat_data$woody_veg_ht1)
-quadrat_data$veg_ht1[is.na(quadrat_data$veg_ht1)] <- 0
-# quadrat_data$veg_ht1 <- as.numeric(quadrat_data$veg_ht1)
+
+summary(filter(quadrat_data, is.na(herb_veg_ht1)))
+quadrat_data$herb_veg_ht1[is.na(quadrat_data$herb_veg_ht1)] <- 0
+
+## check 'pct_green' as character
+unique(quadrat_data$pct_green)
 quadrat_data$pct_green[quadrat_data$pct_green=="<1"] <- 0.5
 quadrat_data$pct_green <- as.numeric(quadrat_data$pct_green)
 
-hist(quadrat_data$pct_green)
-hist(quadrat_data$pct_litter)
 
+## check pct litter, bare, green NA's ####
+filter(quadrat_data, is.na(pct_litter)) %>%
+      select(plot_id, date, quadrat_id, litter_ht1, pct_litter, pct_green, pct_bare)
+## All from the same plots/quadrats, probably the ones we whiffed on recording data.
+
+#+ write out processed data ####
+write_csv(quadrat_data, "data/processed_data//quadrat1m.csv")
+
+
+#+ initial data summaries and comparing sampling effort ####
 quadrat_summaries <- quadrat_data %>%
       group_by(installation, plot_id) %>%
       summarise(
