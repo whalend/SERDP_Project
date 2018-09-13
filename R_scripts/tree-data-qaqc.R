10d1ae480c8843ba55aeb3bf91e3ec53e14da455
stevencab <stevencab@ufl.edu>
2018-09-13 17:31:52 -0400

    Begin processing tree data, Jackson H1 missing.

--- R_scripts/tree-data-qaqc.R
+++ R_scripts/tree-data-qaqc.R
@@ -4,7 +4,46 @@
 #' ## Tree Stem Data
 #+ tree data ####
 tree_data <- read_csv("data/raw_data/trees.csv")
-tree_data_sub <- filter(tree_data, date>"2017-06-01", plot_id != "bland03", plot_id != "bland02")
+### tree_data_sub <- filter(tree_data, date>"2017-06-01", plot_id != "bland03", plot_id != "bland02")
+
+summary(tree_data)
+
+filter(tree_data, is.na(plot_id)) %>% 
+  select(date, installation, plot_id, record_id, stem_id, dbh, tag, species)
+
+tree_data = filter(tree_data, !is.na(plot_id))
+
+filter(tree_data, is.na(dbh)) %>% 
+  select(date, installation, plot_id, record_id, stem_id, dbh, tag, species, health)
+
+### Two tree DBH are to be left NA, trees burned up/downed ###
+
+filter(tree_data, is.na(azimuth), plot_id!="cogon plot") %>% 
+  select(date, installation, plot_id, distance, azimuth, species, tag, dbh)
+
+filter(tree_data, is.na(azimuth), plot_id=="cogon plot") %>% 
+  select(date, installation, plot_id, distance, azimuth, species, tag, dbh)
+
+tree_data$plot_id[tree_data$plot_id=="cogon plot"] <- "theater_cogon"
+
+tree_data <- tree_data %>%
+  mutate(plot_id = paste(installation, plot_id, sep = " "),
+         plot_id=tolower(plot_id)) %>% 
+  filter(date>20170601)
+
+unique(plot_visit_data$plot_id) %in% unique(tree_data$plot_id)
+
+n_distinct(tree_data$plot_id)
+n_distinct(plot_visit_data$plot_id)
+
+summary(tree_data)
+
+unique(tree_data$)
+
+### pause data processing steven ###
+write_csv(tree_data, "data/processed_data/trees.csv")
+       
+##
 
 str(tree_data)
 n_distinct(tree_data$tag)
