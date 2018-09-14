808414e8046e16af4ef48562a601a9314c848123
stevencab <stevencab@ufl.edu>
2018-09-14 14:43:06 -0400

    Processed trees data

--- R_scripts/tree-data-qaqc.R
+++ R_scripts/tree-data-qaqc.R
@@ -1,5 +1,12 @@
 #' # Script for doing QA/QC on tree data
 
+library(plyr); library(dplyr);
+library(readr)
+library(stringi)
+library(ggplot2)
+
+#+ load processed plot visit data ####
+plot_visit_data <- read_csv("data/processed_data/plot_visit_data.csv")
 
 #' ## Tree Stem Data
 #+ tree data ####
@@ -14,15 +21,25 @@ filter(tree_data, is.na(plot_id)) %>%
 tree_data = filter(tree_data, !is.na(plot_id))
 
 filter(tree_data, is.na(dbh)) %>% 
-  select(date, installation, plot_id, record_id, stem_id, dbh, tag, species, health)
+  select(date, plot_id, record_id, stem_id, dbh, tag, species, health)
 
+filter(tree_data, dbh==0) %>% 
+  select(date, plot_id, stem_id, tag, species, dbh)
+
+tree_data$dbh[tree_data$tag=="1138"] <- 15.2
+tree_data$dbh[tree_data$tag=="1139"] <- 14.0
+
+### Corrected two tree DBH that were entered as 0          ###
 ### Two tree DBH are to be left NA, trees burned up/downed ###
 
-filter(tree_data, is.na(azimuth), plot_id!="cogon plot") %>% 
-  select(date, installation, plot_id, distance, azimuth, species, tag, dbh)
+filter(tree_data, dbh<3) %>% 
+  select(date, plot_id, stem_id, tag, species, dbh)
 
-filter(tree_data, is.na(azimuth), plot_id=="cogon plot") %>% 
-  select(date, installation, plot_id, distance, azimuth, species, tag, dbh)
+tree_data$dbh[tree_data$tag=="3056"] <- 25.7
+tree_data$dbh[tree_data$tag=="4395"] <- 11.4
+
+### Changed trees with dbh <3 with estimated values ###
+
 
 tree_data$plot_id[tree_data$plot_id=="cogon plot"] <- "theater_cogon"
 
@@ -31,6 +48,32 @@ tree_data <- tree_data %>%
          plot_id=tolower(plot_id)) %>% 
   filter(date>20170601)
 
+filter(tree_data, is.na(azimuth), plot_id!="blanding theater_cogon") %>% 
+  select(date, stem_id, plot_id, distance, azimuth, species, tag, dbh)
+
+filter(tree_data, is.na(azimuth), plot_id=="blanding theater_cogon") %>% 
+  select(date, stem_id, installation, plot_id, distance, azimuth, species, tag, dbh)
+
+tree_data$azimuth[tree_data$tag=="4092"] <- 320
+tree_data$azimuth[tree_data$tag=="4402"] <- 337
+tree_data$azimuth[tree_data$tag=="4409"] <- 340
+tree_data$azimuth[tree_data$tag=="4380"] <- 27
+tree_data$azimuth[tree_data$tag=="4389"] <- 37
+tree_data$azimuth[tree_data$tag=="4397"] <- 70
+tree_data$azimuth[tree_data$tag=="4396"] <- 91
+tree_data$azimuth[tree_data$tag=="4450"] <- 127
+tree_data$azimuth[tree_data$tag=="4449"] <- 163
+tree_data$azimuth[tree_data$stem_id=="2793"] <- 161
+
+tree_data$azimuth[tree_data$tag=="3447"] <- 23.8
+
+tree_data$azimuth[tree_data$stem_id=="3072"] <- 299.5
+tree_data$azimuth[tree_data$stem_id=="3078"] <- 307.5
+tree_data$azimuth[tree_data$stem_id=="3079"] <- 307.7
+
+## Added azimuth values for trees in Benning A1/J1 and Gordon V1 ##
+## Stem IDs 1199 and 1807 have no azimuth recorded ##
+
 unique(plot_visit_data$plot_id) %in% unique(tree_data$plot_id)
 
 n_distinct(tree_data$plot_id)
@@ -38,12 +81,88 @@ n_distinct(plot_visit_data$plot_id)
 
 summary(tree_data)
 
-unique(tree_data$)
+unique(tree_data$plot_id)
 
-### pause data processing steven ###
+filter(tree_data, is.na(health), plot_id!="blanding theater_cogon") %>% 
+  select(installation, plot_id, date, tag, species, health, canopy)
+
+tree_data$health[tree_data$plot_id=="jackson m1" & tree_data$tag=="3235"] <- 0
+
+tree_data$health[tree_data$tag=="3888"] <- 0
+
+## Added 0 values to tree health for 3235 and 3888 ##
+## No health or canopy entered for tag 4083 ##
+
+
+filter(tree_data, is.na(distance), plot_id!="blanding theater_cogon") %>% 
+  select(installation, plot_id, date, tag, species, distance)
+
+tree_data$distance[tree_data$tag=="1755"] <- 11.5
+
+## Distance -- Changed tag 1755, tag 120 Eglin P1 has no value NA ##
+
+filter(tree_data, is.na(height)) %>% 
+  select(date, plot_id, stem_id, tag, species, dbh, height)
+
+filter(tree_data, is.na(char)) %>% 
+  select(date, plot_id, stem_id, tag, species, dbh, char)
+
+summary(tree_data)
+
+filter(tree_data, distance=="124") %>% 
+  select(date, plot_id, stem_id, tag, species, dbh, distance)
+
+tree_data$distance[tree_data$tag=="1995"] <- 12.4
+
+filter(tree_data, distance=="121") %>% 
+  select(date, plot_id, stem_id, tag, species, dbh, distance)
+
+tree_data$distance[tree_data$tag=="3933"] <- 12.1
+
+filter(tree_data, distance=="101") %>% 
+  select(date, plot_id, stem_id, tag, species, dbh, distance)
+
+tree_data$distance[tree_data$tag=="1996"] <- 10.0
+
+filter(tree_data, distance>12.6) %>% 
+  select(date, plot_id, stem_id, tag, species, dbh, distance, azimuth)
+
+tree_data$distance[tree_data$tag=="1386"] <- 7.3
+tree_data$distance[tree_data$stem_id=="1321"] <- 10.5
+tree_data$distance[tree_data$stem_id=="1383"] <- 10.9
+tree_data$distance[tree_data$tag=="3034"] <- 8.9
+tree_data$distance[tree_data$tag=="4223"] <- 8.3
+tree_data$distance[tree_data$tag=="3775"] <- 5.3
+tree_data$distance[tree_data$stem_id=="1818"] <- 8.1
+
+## Corrected distances over 12.6 due to straight error or no decimal ##
+
+filter(tree_data, height>50) %>% 
+  select(date, plot_id, stem_id, tag, species, dbh, height)
+
+tree_data$height[tree_data$tag=="3650"] <- 7.6
+tree_data$height[tree_data$tag=="4613"] <- 13.2
+
+## Corrected unrealistic high heights that were missing decimal points ##
+
+filter(tree_data, height<2) %>% 
+  select(date, plot_id, stem_id, tag, species, dbh, height)
+
+## Low tree heights of 1.4, 1.4, 0.6 are correct ##
+
+filter(tree_data, char>15) %>% 
+  select(date, plot_id, stem_id, tag, species, dbh, char)
+
+tree_data$char[tree_data$tag=="3076"] <- 0.42
+
+## Changed char from 42 to 0.42 ##
+
+summary(tree_data)
+
 write_csv(tree_data, "data/processed_data/trees.csv")
-       
-##
+
+##Steven stopped processing here
+
 
 str(tree_data)
 n_distinct(tree_data$tag)
