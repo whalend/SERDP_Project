31a0ad51ce014b98f358f716aef8c849b234ca9d
Whalen <whalend@users.noreply.github.com>
2018-09-14 15:11:16 -0400

    A couple of crumbs to follow.

--- R_scripts/dung-data-qaqc.R
+++ R_scripts/dung-data-qaqc.R
@@ -3,3 +3,12 @@
 #' ## Host Abundance Estimates
 #+ dung data ####
 dung_data <- read_csv("data/raw_data/dung.csv")
+
+# filter(plotid %in% c("n","s","e"))
+
+anti_join(plot_visit_data, dung_data)
+
+d <- dung_data %>%
+      group_by(installation, plot_id) %>%
+      summarise(n_transect = n_distinct(location))
+summary(d)
