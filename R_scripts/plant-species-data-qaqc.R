4225e25ec795e4d511a4a838c5f35c3ca9241aa4
stevencab <stevencab@ufl.edu>
2018-09-25 16:21:36 -0400

    Filter species data to each installation

--- R_scripts/plant-species-data-qaqc.R
+++ R_scripts/plant-species-data-qaqc.R
@@ -86,8 +86,6 @@ n_distinct(d2$functional_group)
 sort(unique(species_data$functional_group__1))
 sort(unique(species_data$veg_id))
 
-#### Steven checking processing, looks good ####
-
 #### Steven begin processing for individual installations ####
 
 summary(species_data)
@@ -120,21 +118,124 @@ species_data <- read_csv("data/processed_data/species1m.csv")
 
 summary(species_grouped)
 
+#### Filter for Fort Benning ####
+
 species_benning <- species_grouped %>%
   filter(installation=="benning")
 
 summary(species_benning)
 
-filter(species_data) %>%
-  select(plot_id, veg_id, pct_cover, ht_under50cm, ht50_100cm, ht_over100cm)
-
 species_benning <- filter(species_benning) %>%
   ungroup(.) %>%
-  select(plot_id, date, veg_id, avg_pct_cover, num_stems_m2, species_name, functional_group)
-
-unique(species_benning$veg_id)
+  select(plot_id, date, avg_pct_cover, num_stems_m2, species_name, functional_group)
 
 write_csv(species_benning, "data/processed_by_installation/fort_benning/species_benning.csv")
+
+#### Filter for Camp Blanding ####
+
+species_blanding <- species_grouped %>%
+  filter(installation=="blanding")
+
+summary(species_blanding)
+
+species_blanding <- filter(species_blanding) %>%
+  ungroup(.) %>%
+  select(plot_id, date, avg_pct_cover, num_stems_m2, species_name, functional_group)
+
+write_csv(species_blanding, "data/processed_by_installation/camp_blanding/species_blanding.csv")
+
+#### Filter for Avon Park AFR ####
+
+species_avonpark <- species_grouped %>%
+  filter(installation=="avonpark")
+
+summary(species_avonpark)
+
+species_avonpark <- filter(species_avonpark) %>%
+  ungroup(.) %>%
+  select(plot_id, date, avg_pct_cover, num_stems_m2, species_name, functional_group)
+
+write_csv(species_avonpark, "data/processed_by_installation/avon_park_afr/species_avonpark.csv")
+
+#### Filter for Eglin AFB ####
+
+species_eglin <- species_grouped %>%
+  filter(installation=="eglin")
+
+summary(species_eglin)
+
+species_eglin <- filter(species_eglin) %>%
+  ungroup(.) %>%
+  select(plot_id, date, avg_pct_cover, num_stems_m2, species_name, functional_group)
+
+write_csv(species_eglin, "data/processed_by_installation/eglin_afb/species_eglin.csv")
+
+#### Filter for Tyndall AFB ####
+
+species_tyndall <- species_grouped %>%
+  filter(installation=="tyndall")
+
+summary(species_tyndall)
+
+species_tyndall <- filter(species_tyndall) %>%
+  ungroup(.) %>%
+  select(plot_id, date, avg_pct_cover, num_stems_m2, species_name, functional_group)
+
+write_csv(species_tyndall, "data/processed_by_installation/tyndall_afb/species_tyndall.csv")
+
+#### Filter for Fort Jackson ####
+
+species_jackson <- species_grouped %>%
+  filter(installation=="jackson")
+
+summary(species_jackson)
+
+species_jackson <- filter(species_jackson) %>%
+  ungroup(.) %>%
+  select(plot_id, date, avg_pct_cover, num_stems_m2, species_name, functional_group)
+
+write_csv(species_jackson, "data/processed_by_installation/fort_jackson/species_jackson.csv")
+
+#### Filter for Camp Shelby ####
+
+species_shelby <- species_grouped %>%
+  filter(installation=="shelby")
+
+summary(species_shelby)
+
+species_shelby <- filter(species_shelby) %>%
+  ungroup(.) %>%
+  select(plot_id, date, avg_pct_cover, num_stems_m2, species_name, functional_group)
+
+write_csv(species_shelby, "data/processed_by_installation/camp_shelby/species_shelby.csv")
+
+#### Filter for Fort Gordon ####
+
+species_gordon <- species_grouped %>%
+  filter(installation=="gordon")
+
+summary(species_gordon)
+
+species_gordon <- filter(species_gordon) %>%
+  ungroup(.) %>%
+  select(plot_id, date, avg_pct_cover, num_stems_m2, species_name, functional_group)
+
+write_csv(species_gordon, "data/processed_by_installation/fort_gordon/species_gordon.csv")
+
+#### Filter for Moody AFB ####
+
+species_moody <- species_grouped %>%
+  filter(installation=="moody")
+
+summary(species_moody)
+
+species_moody <- filter(species_moody) %>%
+  ungroup(.) %>%
+  select(plot_id, date, avg_pct_cover, num_stems_m2, species_name, functional_group)
+
+write_csv(species_moody, "data/processed_by_installation/moody_afb/species_moody.csv")
+
+#### Completion of filtering all species to each of 9 installations ####
 
 #################
 #### NOT RUN ####
