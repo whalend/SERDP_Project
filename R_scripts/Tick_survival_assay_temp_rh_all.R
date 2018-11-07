141b90bc5d10e838d5fb64c06a1cc5f5187beb0f
Whalen <whalend@users.noreply.github.com>
2018-11-07 16:04:20 -0500

    Add comments to code.

--- R_scripts/Tick_survival_assay_temp_rh_all.R
+++ R_scripts/Tick_survival_assay_temp_rh_all.R
@@ -1,3 +1,5 @@
+# Combine individual temp/rh logger files into single datagrame ####
+
 library(plyr)
 library(dplyr)
 library(readr)
@@ -8,7 +10,7 @@ library(readr)
 # Logger1b=read.csv("~/Desktop/TSA/temprh01-20381844-20181025.csv")
 # (Logger1a)
 
-pd = "data/raw_data/tick_survival_assay/temp_rh_loggers/"
+pd = "data/raw_data/tick_survival_assay/temp_rh_loggers/" # parent directory
 
 file_list <- list.files("data/raw_data/tick_survival_assay/temp_rh_loggers/", pattern = "csv")
 
@@ -17,20 +19,22 @@ file_list <- list.files("data/raw_data/tick_survival_assay/temp_rh_loggers/", pa
 
 # length(stringr::str_sub(f, start = 7, end = 8))
 
-col_names <- c("date","time","tempC","RH")
-df1 <- data.frame()
+col_names <- c("date","time","tempC","RH")# column names
+df1 <- data.frame()# empty dataframe
+
+## Function to loop through and combine files
 for(f in file_list){
   n = read_csv(paste(pd,f,sep = ""))
   n = n[,c(1:4)]
   names(n) <- col_names
-  n$logger_id = paste(stringr::str_sub(f, start = 7, end = 8)) 
+  n$logger_id = paste(stringr::str_sub(f, start = 7, end = 8))
   df1 <- rbind(df1, n)
 }
 
 head(df1)
 unique(df1$logger_id)
 summary(df1)
-df1 <- filter(df1, is.na(tempC)==FALSE)
+df1 <- filter(df1, is.na(tempC)==FALSE)# remove NA rows from data
 
 write_csv(df1, "data/processed_data/tick_survival_combined_temperature_rh.csv")
 
