c63b6d0b2ca76b1665aa4f2161700becf69f1971
Whalen <whalend@users.noreply.github.com>
2019-02-04 15:27:20 -0500

    Reorder field names.

--- R_scripts/Eglin_GIS.R
+++ R_scripts/Eglin_GIS.R
@@ -27,7 +27,9 @@ eglin_fires <- eglin_fires %>%
                    TRUE ~ "dormant"
              ),
              purpose = NA
-      )
+      ) %>%
+      select(inst_name:fDate, season, purpose, fYear, fMonth)
+eglin_fires
 
 # filter(eglin_fires, season=="dormant")$fMonth
 # str(eglin_fires)
@@ -41,33 +43,31 @@ eglin_fires <- eglin_fires %>%
 # detach("package:lubridate", unload=TRUE)
 
 ## Eglin Rx Fires (Not Run) ####
-eglin_rxfires <- eglin_fires[eglin_fires$FIRETYPE=="prescribed",]
-summary(eglin_rxfires)
-plot(eglin_rxfires[eglin_rxfires$fire_year > 2013,], col = eglin_rxfires$fire_year)
-# writeOGR(eglin_rxfires, "data/Eglin-AFB/", "eglin_rxfires", driver = "ESRI Shapefile")
-
-# shapefiles for each year
-yr <- seq(2006,2018,1)
-for(y in yr){
-      df <- eglin_rxfires[eglin_rxfires@data$fire_year == y,]
-      writeOGR(df,
-               dsn = "data/Eglin-AFB/rxfire_by_year/",
-               layer = paste("Eglin_rxfire", y, sep=""),
-               driver = "ESRI Shapefile", overwrite_layer = T)
-
-}
-
-shp_list <- list.files("data/Eglin-AFB/rxfire_by_year/", pattern = ".shp")
-shp_list <- strsplit(shp_list, ".shp")
-for(shp in shp_list){
-      assign(shp,
-             readOGR(dsn = "data/Eglin-AFB/rxfire_by_year/", layer = shp))
-}
-
-plot(`Eglin-rxfire2015`)
-plot(`Eglin-rxfire2006`)
-plot(gDifference(`Eglin-rxfire2006`, `Eglin-rxfire2015`), add = T, col = "blue")
-plot(`Eglin-rxfire2015`, add = T, col = "orange")
-
-
+# eglin_rxfires <- eglin_fires[eglin_fires$FIRETYPE=="prescribed",]
+# summary(eglin_rxfires)
+# plot(eglin_rxfires[eglin_rxfires$fire_year > 2013,], col = eglin_rxfires$fire_year)
+# # writeOGR(eglin_rxfires, "data/Eglin-AFB/", "eglin_rxfires", driver = "ESRI Shapefile")
+#
+# # shapefiles for each year
+# yr <- seq(2006,2018,1)
+# for(y in yr){
+#       df <- eglin_rxfires[eglin_rxfires@data$fire_year == y,]
+#       writeOGR(df,
+#                dsn = "data/Eglin-AFB/rxfire_by_year/",
+#                layer = paste("Eglin_rxfire", y, sep=""),
+#                driver = "ESRI Shapefile", overwrite_layer = T)
+#
+# }
+#
+# shp_list <- list.files("data/Eglin-AFB/rxfire_by_year/", pattern = ".shp")
+# shp_list <- strsplit(shp_list, ".shp")
+# for(shp in shp_list){
+#       assign(shp,
+#              readOGR(dsn = "data/Eglin-AFB/rxfire_by_year/", layer = shp))
+# }
+#
+# plot(`Eglin-rxfire2015`)
+# plot(`Eglin-rxfire2006`)
+# plot(gDifference(`Eglin-rxfire2006`, `Eglin-rxfire2015`), add = T, col = "blue")
+# plot(`Eglin-rxfire2015`, add = T, col = "orange")
 
