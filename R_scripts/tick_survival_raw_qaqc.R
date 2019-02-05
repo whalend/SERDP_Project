1751811b179e73f1197c732033ec32f2f2c589cf
Whalen <whalend@users.noreply.github.com>
2019-02-05 16:03:45 -0500

    Add alive and dead numbers to data frame.

--- R_scripts/tick_survival_raw_qaqc.R
+++ R_scripts/tick_survival_raw_qaqc.R
@@ -29,7 +29,7 @@ tick_survival <- tick_survival %>%
 
 colnames(tick_survival) <- c("LocationID", "Tag", "Invaded", "Date", "JulianDay", "nymphs_alive", "nymphs_dead", "nymph_survival", "adult_F_alive", "adult_F_dead", "adult_F_survival", "adult_M_alive", "adult_M_dead", "adult_M_survival", "Avg CC", "NOTES", "days")
 
-range(tick_survival$days)
+# range(tick_survival$days)
 
 tick_survival_long <- tick_survival %>%
   select(LocationID:JulianDay, days, nymph_survival, adult_F_survival, adult_M_survival) %>%
@@ -44,7 +44,38 @@ tick_survival_long <- tick_survival %>%
                str_detect(life_stage, "adult") ~ "adult",
                str_detect(life_stage, "nymph") ~ "nymph"
          ))
+
+alive_long <- tick_survival %>%
+      select(LocationID:JulianDay, days, nymphs_alive, adult_F_alive, adult_M_alive) %>%
+      gather(., key = life_stage, value = alive, -LocationID:-days) %>%
+      mutate(Invaded = ifelse(Invaded == "Yes", "invaded", "native"),
+             sex = case_when(
+                   str_detect(life_stage, "adult_M") ~ "male",
+                   str_detect(life_stage, "adult_F") ~ "female",
+                   str_detect(life_stage, "nymph") ~ "nymph"
+             ),
+             life_stage = case_when(
+                   str_detect(life_stage, "adult") ~ "adult",
+                   str_detect(life_stage, "nymph") ~ "nymph"
+             ))
+
+dead_long <- tick_survival %>%
+      select(LocationID:JulianDay, days, nymphs_dead, adult_F_dead, adult_M_dead) %>%
+      gather(., key = life_stage, value = dead, -LocationID:-days) %>%
+      mutate(Invaded = ifelse(Invaded == "Yes", "invaded", "native"),
+             sex = case_when(
+                   str_detect(life_stage, "adult_M") ~ "male",
+                   str_detect(life_stage, "adult_F") ~ "female",
+                   str_detect(life_stage, "nymph") ~ "nymph"
+             ),
+             life_stage = case_when(
+                   str_detect(life_stage, "adult") ~ "adult",
+                   str_detect(life_stage, "nymph") ~ "nymph"
+             ))
+tick_survival_long <- join_all(list(alive_long,dead_long,tick_survival_long))
 summary(tick_survival_long)
+
+filter(tick_survival_long, life_stage == "adult", dead >5)
 # unique(tick_survival_long$life_stage)
 # unique(tick_survival_long$sex)
 
@@ -71,6 +102,8 @@ add_visit_number <- data.frame(days = unique(tick_survival$days), visit_number= 
 
 tick_survival <- left_join(tick_survival, add_visit_number)
 tick_survival_long <- left_join(tick_survival_long, add_visit_number)
+
+write_csv(tick_survival_long, "data/processed_data/tick_survival_long.csv", append = T)
 
 # View(tick_survival)
 
